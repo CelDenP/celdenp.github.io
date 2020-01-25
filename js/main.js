9d814f9042fffb870d2410607b75eb3b3b30bfde
CelDenP <48330778+CelDenP@users.noreply.github.com>
2020-01-25 04:00:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 600,
+    spaceBetween: 100,
+    pagination: {
+    el: '.project-pagination',
+    type: 'bullets',
+    clickable: true,
+},
+	// autoplay: {
+ //    delay: 5000,
+ //  },
+});
